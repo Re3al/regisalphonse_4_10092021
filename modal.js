a8d2996ea9e3580cd56aff68b08e57d72820ae1a
Regis Alphonse <regisalphonse@gmail.com>
2021-09-23 21:00:26 +0200

    Optimsation formulaire

--- modal.js
+++ modal.js
@@ -25,12 +25,22 @@ function launchModal() {
 
 //submit btn
 const answer = document.getElementById('form-answer');
+const wrongAnswer = document.getElementById('form-error');
+//const formEntier = document.querySelector("#formInscription");
+//const inputs = container.querySelectorAll("input").addEventListener('submit', function(e){
 document.forms["formInscription"].addEventListener('submit', function(e){
   let error;
+
   //récupère l'objet écouté 
   let inputs = this;
    for(i=0;i<inputs.length;i++)
    {
+    lenghtName = inputs["last"].value;
+    lenghtName = lenghtName.length;
+      if(lenghtName < 2)
+      {
+    error ="Veuillez entrer 2 caractères ou plus pour le champ du nom";
+      }
      if(!inputs[i].value)
      {
        error = "Veuillez renseigner tous les champs";
@@ -41,11 +51,11 @@ document.forms["formInscription"].addEventListener('submit', function(e){
    if(error)
    {
     e.preventDefault();
-    answer.innerHTML = error;
+    wrongAnswer.innerHTML = error;
     return false;
    }
    else{
-    answer.innerHTML ="Formulaire validé !";
+    answer.innerHTML ="Merci ! Votre réservation a été reçue";
     e.preventDefault();
    }
 
