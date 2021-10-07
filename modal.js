d1ab3d8739a2b89541bea74b2466b53a36a270d2
Regis Alphonse <regisalphonse@gmail.com>
2021-10-07 21:03:07 +0200

    iSSUE 5

--- modal.js
+++ modal.js
@@ -6,7 +6,6 @@ function editNav() {
     x.className = "topnav";
   }
 }
-console.log('test22');
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -25,17 +24,19 @@ function launchModal() {
 //close modal
 function closeModal() {
   modalbg.style.display = "none";
-  console.log('modal clicked');
 }
 //validation message
-
+function validate()
+{
+  alert('Valider');
+}
 
 
 //submit btn
 const answer = document.getElementById('form-answer');
 const wrongAnswer = document.getElementById('form-error');
 document.forms["formInscription"].addEventListener('submit', function(e){
-  let error;
+  let error = [];
   //index de l'erreur
   let indexError = 0;
   //récupère l'objet écouté 
@@ -44,60 +45,67 @@ document.forms["formInscription"].addEventListener('submit', function(e){
 let errorIndex = []; 
 
 
+  
+lenghtName = inputs["last"].value;
+lenghtName = lenghtName.length;
+
+
+
+  lenghtFirstname = inputs["first"].value;
+lenghtFirstname = lenghtFirstname.length;
+
+if(lenghtFirstname < 2)
+{
+error.push("Veuillez entrer 2 caractères ou plus pour le champ du prénom");
+indexError = 0;
+errorIndex.push(0);
+}
+
+
+
+if(lenghtName < 2)
+{
+error.push("Veuillez entrer 2 caractères ou plus pour le champ du nom");
+indexError = 1;
+errorIndex.push(1);
+
+}
+
   if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(!inputs['email'].value))
   {  
-    error = "Veuillez renseigner une adresse email valide";
+    error.push( "Veuillez renseigner une adresse email valide");
     indexError = 2;
     errorIndex.push(2);
     // break;
   }
-  
-    lenghtName = inputs["last"].value;
-    lenghtName = lenghtName.length;
+
     
-      if(lenghtName < 2)
-      {
-    error ="Veuillez entrer 2 caractères ou plus pour le champ du nom";
-    indexError = 1;
-    errorIndex.push(1);
-
-      }
-
-      lenghtFirstname = inputs["first"].value;
-    lenghtFirstname = lenghtFirstname.length;
-    
-      if(lenghtFirstname < 2)
-      {
-    error ="Veuillez entrer 2 caractères ou plus pour le champ du prénom";
-    indexError = 0;
-    errorIndex.push(0);
-      }
-
+    if(!inputs['birthdate'].value)
+    {
+      error.push( "Vous devez entrer votre date de naissance");
+      indexError = 3;
+      errorIndex.push(3);
+      // break;
+    }
   
      if((!inputs['quantity'].value))
      {
-       error = "Vous devez saisir une valeur numérique";
+       error.push( "Vous devez saisir une valeur numérique");
        indexError = 4;
        errorIndex.push(4);
        // break;
      }
      if(!inputs['location'].value)
      {
-       error = "Vous devez choisir une option.";
+       error.push( "Vous devez choisir une option.");
        indexError = 5;
        errorIndex.push(5);
        // break;
      }
-     if(!inputs['birthdate'].value)
-     {
-       error = "Vous devez entrer votre date de naissance";
-       indexError = 3;
-       errorIndex.push(3);
-       // break;
-     }
+
      if(document.querySelector('#checkbox1:checked') == null)
      {
-       error = "Vous devez vérifier que vous acceptez les termes et conditions.";
+       error.push( "Vous devez vérifier que vous acceptez les termes et conditions.");
        indexError = 6;
        errorIndex.push(6);
        // break;
@@ -117,7 +125,7 @@ let errorIndex = [];
     errorMessage.innerHTML = error;*/
     blocInput.forEach( function (el,index)
     {
-
+      console.log(index);
       const blocInput = document.querySelectorAll('.modal-body .formData');
       let a = errorIndex.findIndex(function(value)
       {
@@ -126,19 +134,25 @@ let errorIndex = [];
 
       if(a >= 0)
       {
+        //console.log(error);
+        //console.log(a + "=>"+ error);
         targetInput = el.querySelector('input');
         targetInput.classList.add("error-form");
-
-            /*const errorMessage = document.createElement("p");
-    console.log(blocInput[indexError]);
-    console.log(indexError);
-    blocInput[indexError].appendChild(errorMessage);
-    errorMessage.innerHTML = error;
+        let errorMessage = document.createElement("p");
+        blocInput[a].appendChild(errorMessage);
+        errorMessage.classList.add("error-message");
+    errorMessage.innerHTML = error[a];
+        
       }
+
       else 
       {
         targetInput = el.querySelector('input');
         targetInput.classList.remove("error-form");
+        errorMessage.classList.remove("error-message");
+        targetInput.removeChild(errorMessage);
+        errorMessage.classList.add("test-message");
+        console.log(errorMessage + 'erreur résolue');
       }
       
       
@@ -160,7 +174,9 @@ let errorIndex = [];
    }
    else{
     answer.innerHTML ="Merci ! Votre réservation a été reçue";
-    e.preventDefault();
+    //e.preventDefault();
+    alert('test');
+    console.log('envoie validé');
    }
 
 });
